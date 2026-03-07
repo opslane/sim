3e94ce32e83387018d6abff49e8c124d0630e0c2
Theodore Li <teddy@zenobiapay.com>
2026-03-06 19:55:53 -0800

    Fix test

--- apps/sim/providers/utils.test.ts
+++ apps/sim/providers/utils.test.ts
@@ -1402,6 +1402,7 @@ describe('prepareToolExecution', () => {
         workspaceId: 'ws-456',
         chatId: 'chat-789',
         userId: 'user-abc',
+        skipFixedUsageLog: true,
       })
     })
 
